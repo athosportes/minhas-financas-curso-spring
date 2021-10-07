da12f5f90dec2a9cabe599d271fff58a2c9ca78c
Athos Roberto de Souza Pereira Portes <athos.portes@meta.com.br>
2021-10-06 22:08:40 -0300

    controller crated

--- src/main/java/com/athosportes/controller/UsuarioController.java
+++ src/main/java/com/athosportes/controller/UsuarioController.java
@@ -1,26 +1,39 @@
 package com.athosportes.controller;
 
+import com.athosportes.dto.UsuarioDTO;
+import com.athosportes.exception.RegraNegocioException;
 import com.athosportes.model.entity.Usuario;
 import com.athosportes.model.repository.UsuarioRepository;
+import com.athosportes.service.UsuarioService;
 import com.athosportes.service.impl.UsuarioServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/minhas-financas")
 @AllArgsConstructor
 @NoArgsConstructor
+@RequestMapping("/minhas-financas")
 public class UsuarioController {
 
     private UsuarioRepository repository;
-    private UsuarioServiceImpl service;
+    private UsuarioService service;
 
     @PostMapping
-    private Usuario salvarUsuario(@RequestBody Usuario usuario) {
-        return service.salvarUsuario(usuario);
+    private ResponseEntity salvarUsuario(@RequestBody UsuarioDTO dto) {
+
+        Usuario usuario = Usuario.builder()
+                .nome(dto.getNome())
+                .senha(dto.getEmail())
+                .senha(dto.getSenha())
+                .build();
+        try {
+            Usuario usuarioSalvo = service.salvarUsuario(usuario);
+            return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+        } catch (RegraNegocioException ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
     }
 }
